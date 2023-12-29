709aea9dbf595a2a0c29e9a0652f56ff0cb8769d
MOHIT-D360 <mohitnsp786@gmail.com>
2023-12-30 00:02:04 +0530

    home page done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './layout/home/Home';
+import About from './layout/about/About';
+import Job from './layout/job-book/Job';
+import Photo from './layout/photo/Photo';
+import Nav from './layout/nav/Nav';
 
 function App() {
   return (
     <div className="App">
-     
+     <BrowserRouter>
+     <Nav/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route exact path="/job" element={<Job/>}/>
+        <Route exact path="/photo" element={<Photo/>}/>
+      </Routes>
+     </BrowserRouter>
     </div>
   );
 }
